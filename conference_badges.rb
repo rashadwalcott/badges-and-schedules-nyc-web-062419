0883e31400b3fdd5398aeb440a981d0e89304851
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 06:41:39 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+  list=[]
+  badges.each do |badge|
+ list << (badge_maker(badge))
+end
+list
+end
+
+def assign_rooms(attendees)
+  
+end

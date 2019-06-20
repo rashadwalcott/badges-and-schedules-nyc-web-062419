9f464ada1aa1df91479071a54225483470f9b8bd
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 21:50:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,19 @@ list
 end
 
 def assign_rooms(attendees)
-  
+  room_assignment = Array (1..7)
+  message=[];
+ attendees.each_with_index do |attendee,index|
+   message << "Hello, #{attendee}! You'll be assigned to room #{room_assignment[index]}!"
+end
+  message
+end
+
+def printer(attendees)
+ batch_badge_creator(attendees).each do |attendee|
+   puts attendee
+ end
+assign_rooms(attendees).each do |attendee|
+  puts attendee
+end
 end

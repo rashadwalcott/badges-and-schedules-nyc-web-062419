69dcd45b0a680bb51f280c6c7872a23f8b6f753b
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 21:06:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignment = Array (1..7)
   
-  room_assignment.each_with_index {|item,index| "Hello,#{attendees[index]}! You'll be assigned to room #{room_assignment[index]}!"}
+  room_assignment.each_with_index {|item,index| "Hello,#{attendees[item]}! You'll be assigned to room #{room_assignment[item]}!"}
 end
 =begin message=[]
   attendees.each do |attend|
